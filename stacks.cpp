d4335aa09b0e82aee8e77199b6b6aa9b482dbfec
vbanasevych <viktoriabanasevic@gmail.com>
2025-09-20 23:11:17 +0300

    створено stack.h та в stack.ccp реалізовано методи для статистики

---
+++ stacks.cpp
@@ -0,0 +1,71 @@
+#include "stacks.h"
+#include <algorithm>
+#include <iostream>
+#include <stdexcept>
+
+using namespace std;
+
+StackAnalyzer::StackAnalyzer(int cardsPerSuit, int n)
+    : cardsPerSuit(cardsPerSuit), n(n) {
+    if (cardsPerSuit <= 0 || n <= 0) {
+        throw invalid_argument("cardsPerSuit and n must be positive");
+    }
+}
+
+double StackAnalyzer::average() const {
+    if (stackLengths.empty()) return 0.0;
+
+    double sum = 0.0;
+    for (int len: stackLengths) {
+        sum += len;
+    }
+    return sum / stackLengths.size();
+}
+
+
+double StackAnalyzer::median() const {
+    if (stackLengths.empty()) return 0.0;
+
+    vector<int> temp = stackLengths;
+    sort(temp.begin(), temp.end());
+    size_t mid = temp.size() / 2;
+
+    if (temp.size() % 2 == 0) {
+        return (temp[mid - 1] + temp[mid]) / 2.0;
+    }
+    return temp[mid];
+}
+
+int StackAnalyzer::mode() const {
+    if (stackLengths.empty()) return 0;
+
+    map<int, int> counts;
+    for (int len: stackLengths) ++counts[len];
+
+    int maxCount = 0;
+    int modeVal = 0;
+    for (const auto &pair : counts) {
+        if (pair.second > maxCount) {
+            maxCount = pair.second;
+            modeVal = pair.first;
+        }
+    }
+    return modeVal;
+}
+
+map<int, double> StackAnalyzer::frequency() const {
+    map<int, double> freq;
+
+    if (stackLengths.empty()) return freq;
+
+    map<int, int> counts;
+    for (int len : stackLengths) ++counts[len];
+
+    for (auto it = counts.begin(); it != counts.end(); ++it) {
+        int len = it->first;
+        int count = it->second;
+        freq[len] = static_cast<double>(count) * 100 / stackLengths.size();
+    }
+
+    return freq;
+}
