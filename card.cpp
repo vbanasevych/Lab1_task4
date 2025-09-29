bee05a9b449b7480887f85685746ccc7dcb29578
Bohdan Mihunov <meizu.com.b@gmail.com>
2025-09-29 23:22:47 +0300

    fixed card and added deck dealer

--- card.cpp
+++ card.cpp
@@ -1,14 +1,18 @@
 #include "card.h"
 
-std::ostream& operator<<(std::ostream& os, const Card& c) {
+std::string Card::toString() const {
     std::string suitStr;
-    switch (c.suit) {
+    switch (suit) {
         case Suit::Hearts:   suitStr = "♥"; break;
         case Suit::Diamonds: suitStr = "♦"; break;
         case Suit::Clubs:    suitStr = "♣"; break;
         case Suit::Spades:   suitStr = "♠"; break;
     }
-    os << c.value << suitStr;
+    return std::to_string(value) + suitStr;
+}
+
+std::ostream& operator<<(std::ostream& os, const Card& c) {
+    os << c.toString();
     return os;
 }
 // зробити декомпозицію
---
+++ deck_dealer.h
@@ -0,0 +1,21 @@
+
+#pragma once
+
+#include "card.h"
+#include <vector>
+#include <random>
+
+class DeckDealer {
+    std::vector<Card> deck;
+    size_t index;
+    int cardsPerSuit;
+    std::mt19937 rng;
+
+public:
+    DeckDealer(int cps);
+
+    Card operator()();
+
+private:
+    void newDeck();
+};
